78febdb0bf61cbf4470833dea64fe307bafc526e
oreakinwole <oreakinwole@gmail.com>
2021-12-22 19:09:12 +0100

    chore: scaffold project

---
+++ store/index.js
@@ -0,0 +1,14 @@
+// import cookie from 'cookie'
+export const state = () => ({
+  process: '',
+})
+
+export const mutations = {
+  SET_PROCESS(state, payload) {
+    state.process = payload
+  },
+}
+
+export const actions = {
+
+}
