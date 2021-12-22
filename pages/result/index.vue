b5dd3e06c7fa6f29ff82efeede0c26ff0ecd190a
oreakinwole <oreakinwole@gmail.com>
2021-12-22 21:05:37 +0100

    feat: result page logic

---
+++ pages/result/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="m-auto">
+    <p>Your answers were submitted successfully</p>
+    <span>Your score: {{ finalResult.correctAnswers }} / {{ totalQuestions }} </span>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'ResultPage',
+
+  computed: {
+    ...mapGetters(['finalResult']),
+  },
+}
+</script>
+
+<style></style>
