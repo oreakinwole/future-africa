3a5eb0a12ba296d009011c01726b3c7700bdd161
oreakinwole <oreakinwole@gmail.com>
2021-12-22 19:10:03 +0100

    feat: add home content and link to questions page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,9 @@
 <template>
-<div></div>
+  <div>
+    <p>This is the home page for the future africa challenge</p>
+
+    <nuxt-link to="/questions">Go to questions page</nuxt-link>
+  </div>
 </template>
 
 <script>
